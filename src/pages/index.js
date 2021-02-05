9541ce73577348823517c7fa3da15a3d12a99de4
Pau Larrea <paularrea12@hotmail.com>
2021-02-05 15:59:16 +0100

    divs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,40 @@
-import React from "react"
+import React from "react";
 
-import Layout from "../components/layout/layout"
-import SEO from "../components/seo"
-import styles from "../styles/index.module.scss"
+import Layout from "../components/layout/layout";
+import SEO from "../components/seo";
+import styles from "../styles/index.module.scss";
 
-import Intro from "../components/1.intro/intro"
-import Infography from "../components/2.Infography/infography"
-import ProduktDesign from "../components/3.produktDesign/produktDesign"
-import Produktion from "../components/4.produktion/produktion"
-import Importieren from "../components/5.importieren/importieren"
-import Logistik from "../components/6.logistik/logistik"
-import Fulhen from "../components/7.fuhlen/fuhlen"
-import ContactForm from "../components/8.Contact/form"
-import Blog from  "../components/9.Blog/blog"
+import Intro from "../components/1.intro/intro";
+import Infography from "../components/2.Infography/infography";
+import ProduktDesign from "../components/3.produktDesign/produktDesign";
+import Produktion from "../components/4.produktion/produktion";
+import Importieren from "../components/5.importieren/importieren";
+import Logistik from "../components/6.logistik/logistik";
+import Fulhen from "../components/7.fuhlen/fuhlen";
+import ContactForm from "../components/8.Contact/form";
+import Blog from "../components/9.Blog/blog";
 
 const IndexPage = () => (
-    <div className={styles.website}>
-      <Layout>
-        <SEO title="Home" />
+  <div className={styles.website}>
+    <Layout>
+      <SEO title="Home" />
+      <div>
         <Intro />
+      </div>
+      <div>
         <Infography />
+      </div>
+      <div>
         <ProduktDesign />
-        <Produktion />
-        <Importieren />
-        <Logistik />
-        <Fulhen />
-        <ContactForm />
-        <Blog />
-      </Layout>
-    </div>
-)
+      </div>
+      <Produktion />
+      <Importieren />
+      <Logistik />
+      <Fulhen />
+      <ContactForm />
+      <Blog />
+    </Layout>
+  </div>
+);
 
-export default IndexPage
+export default IndexPage;
